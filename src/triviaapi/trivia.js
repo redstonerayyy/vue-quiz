b18ceee8bdf8dca4a5ea20b297d62f6740e548db
Redstonerayy <antonpaulr@protonmail.com>
2022-01-19 11:42:49 +0100

    added previous trivia code

---
+++ src/triviaapi/trivia.js
@@ -0,0 +1,36 @@
+class TriviaAPI {
+    constructor() {
+        this.urls = {
+            baseurl: "https://opentdb.com/api.php",
+            tokenurl: "https://opentdb.com/api_token.php",
+            categoryurl: "https://opentdb.com/api_category.php",
+            categoryinfourl: "https://opentdb.com/api_count.php",
+            globalinfourl: "https://opentdb.com/api_count_global.php"
+        };
+    }
+
+    getToken() {
+
+    }
+
+    resetToken() {
+
+    }
+
+    getCategories() {
+
+    }
+
+    getCategoryInfo() {
+
+    }
+
+    getGlobalInfo() {
+
+    }
+
+    makeURL(amount, category, difficulty) {
+
+    }
+
+}
